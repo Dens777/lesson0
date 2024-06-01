293e0c04378d09c7885206e13ff19db0459919cc
Dens777 <dens77712@gmail.com>
2024-06-01 17:16:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+a=("Urban University") # Длина слова
+b=len(a)
+print (b)
+first=9 # Суммы и разности
+second=198
+summa=first+second
+diff=first-second
+print(summa,diff)
+first=9 # Среднее арифметическое
+second=198
+third=35
+mean=(first+second+third)//3
+print(mean)
+first_string=("Вторник") # Простые строчки
+second_string=("Понедельник")
+print(second_string,", ",first_string)
+a=10 # Сложная формула
+b=8
+c=79
+f=(((a*b)+(a*c)**3)/2)
+print(f)
